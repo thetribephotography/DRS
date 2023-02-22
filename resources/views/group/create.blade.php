6e6b99cd295f9a6bd6bef4090fa3b48df8440e0c
Egbeleke Daniel <www.daniko15@gmail.com>
2023-02-23 00:25:55 +0100

    minor bug fixes

--- resources/views/group/create.blade.php
+++ resources/views/group/create.blade.php
@@ -1,44 +1,33 @@
 <x-app-layout>
-
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create Group') }}
-        </h2>    
+            Create Group
+        </h2>
     </x-slot>
-
-    <div class="container px-4 px-4 mb-4 mt-4">
-          <div class="mt-10 sm:mt-0">
-    <div class="md:grid md:grid-cols-3 md:gap-6">
-        <div class="w-full max-w-sm p-4 bg-gray border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 gray:bg-gray-800 gray:border-gray-700">
-
-<form>
-  <div class="mb-6">
-    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Group Name</label>
-    <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Input Name" required>
-  </div>
-  <div class="mb-6">
-    <label for="members" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Add Group Members</label>
-    <input type="text" id="members" multiple class="bg-gray-50 border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Input Group Members" required>
-  </div>
-
-  <input type="text" data-role="taginput">
-
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-</form>
-
+    <section>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm">
+                    <form action="{{ route ('group.create') }}" method="POST" class="p-6">
+                        @csrf
+                        <div class="mb-6">
+                          <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Group Name</label>
+                          <input  name="name" type="text" id="name" class="bg-gray-50 border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700" placeholder="Name" required>
+                        </div>
+                        <div class="mb-6">
+                          <label for="members" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Add Group Members</label>
+                          <input name="members" type="text" id="members" class="bg-gray-50 border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700" placeholder="Input Group Members" required>
+                        </div>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-    </div>
+    </section>
 
     <script>
-        $('[data-role="taginput"]').on("keyup", function (e) {
-    if (e.keyCode === 188) {
-        // 188 is the keycode for ','
-        var tags = $(this).val().split(",");
-        console.log(tags);
-    }
-});
+        //
     </script>
-
 </x-app-layout>
